f124130fc371e7704d95933a2785552cd4a565b2
Meenakshi Gaur <136475194+Connectmgaur@users.noreply.github.com>
2023-08-31 19:16:41 +0000

    Pending changes exported from your codespace

---
+++ src/js/component/SimpleCounter.jsx
@@ -0,0 +1,58 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+import propTypes from "prop-types";
+import { useState } from "react";
+
+// include your styles into the webpack bundle
+//import "../styles/index.css";
+
+
+//import your own components
+//import Home from "./component/home.jsx";
+
+function SimpleCounter(props){
+
+    
+    return (
+        
+        <div className="Container">
+            <div className="bigCounter">
+                <div className="timer">
+                    <i class="fas fa-clock"></i>
+                </div>   
+             
+                <div className="four">{props.digitFour}</div>
+                <div className="three">{props.digitThree}</div>
+                <div className="two">{props.digitTwo}</div>
+                <div className="one">{props.digitOne}</div>
+            </div>
+            <button className="btn" onClick={(()=> SetValue=(value+1))}>Increase</button>
+            <button className="btn" onClick={(()=> SetValue=(value-1))}>Decrease</button>
+            <button className="btn" onClick={(()=> SetValue=(0))}>Reset</button>
+            </div>
+
+        );
+}
+SimpleCounter.propTypes={
+
+    digitFour:propTypes.number,
+    digitThree:propTypes.number,
+    digitTwo:propTypes.number,
+    digitOne: propTypes.number
+};
+
+let counter=0
+setInterval (function(){
+   
+    const four=Math.floor(counter/1000);
+    const three=Math.floor(counter/100);
+    const two= Math.floor(counter/10);
+    const one=Math.floor(counter/1);
+    counter++;
+ 
+    ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app"));
+
+},1000);
+
+
